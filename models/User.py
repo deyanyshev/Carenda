8efdab1cb0fb3a79bb9ecd27bce018974894aa62
Dmitry V. Deyanyshev <dddeyanyshev@gmail.com>
2025-10-23 23:59:02 +0300

    TASK-6: added Rental

--- models/User.py
+++ models/User.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+from models.Rental import Rental
+
+
 class User:
     """Класс пользователя системы каршеринга"""
 
@@ -11,3 +16,24 @@ class User:
         # История и текущая аренда
         self.rental_history = []  # Список предыдущих аренд (история поездок)
         self.current_rental = None  # Текущая активная аренда (None если нет активной аренды)
+
+    def rent_transport(self, transport):
+        if self.current_rental:
+            return False  # Уже есть активная аренда
+
+        if not transport.is_available:
+            return False  # Транспорт недоступен
+
+        transport.is_available = False
+        self.current_rental = Rental(transport)
+        return True
+
+    def end_rental(self):
+        if not self.current_rental:
+            return False
+
+        self.current_rental.transport.is_available = True
+        self.current_rental.end_date = datetime.now()
+        self.rental_history.append(self.current_rental)
+        self.current_rental = None
+        return True
